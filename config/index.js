c4f4b78cbc19d2be26d5594dd5b09ca20e10d721
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-04 21:33:54 +0700

    add db config

--- config/index.js
+++ config/index.js
@@ -9,4 +9,11 @@ module.exports = {
     key: process.env.APP_KEY || 'secret',
   },
   env: process.env.NODE_ENV || 'production',
+  db: {
+    dialect: process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'tods',
+  },
 };
