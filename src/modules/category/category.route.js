55a51488e8ca1ca4941a578e482be32faf9e7ecd
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-05 21:31:21 +0700

    add category route

--- src/modules/category/category.route.js
+++ src/modules/category/category.route.js
@@ -1,10 +1,20 @@
 const { Router } = require('express');
 const CategoryController = require('./category.controller.js');
+const CategoryRequest = require('./requests');
 const { auth } = require('../auth/middlewares');
 
 const router = Router();
 
-router.get('/', auth, CategoryController.get);
+router
+  .route('/')
+  .get(auth, CategoryController.get)
+  .post(auth, CategoryRequest.create, CategoryController.create);
+
+router
+  .route('/:id')
+  .get(auth, CategoryController.find)
+  .patch(auth, CategoryRequest.update, CategoryController.update)
+  .delete(auth, CategoryController.delete);
 
 module.exports = {
   path: '/api/categories',
