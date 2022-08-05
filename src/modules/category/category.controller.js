7b0ec84148b12685831f3460c9160804ca50a900
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-05 21:31:05 +0700

    add category controller

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -4,9 +4,66 @@ const { MasterCategory } = require('./services');
 module.exports = class CategoryController {
   static async get(req, res, next) {
     try {
-      const categories = await MasterCategory.get();
+      const categories = await MasterCategory.get({
+        filter: {
+          userId: req.user.id,
+        },
+      });
 
-      new Response.SuccessResponse('', categories).use(res);
+      return new Response.SuccessResponse('', categories).use(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async create(req, res, next) {
+    try {
+      const category = await MasterCategory.create({
+        ...req.body,
+        userId: req.user.id,
+      });
+
+      return new Response.CreatedResponse('', category).use(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async find(req, res, next) {
+    try {
+      const category = await MasterCategory.findWithTodo(req.params.id);
+
+      req.user.canAccessCategory(category);
+
+      return new Response.SuccessResponse('', category).use(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async update(req, res, next) {
+    try {
+      const category = await MasterCategory.find(req.params.id);
+
+      req.user.canAccessCategory(category);
+
+      await MasterCategory.update(category, req.body);
+
+      return new Response.SuccessResponse('', category).use(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async delete(req, res, next) {
+    try {
+      const category = await MasterCategory.find(req.params.id);
+
+      req.user.canAccessCategory(category);
+
+      await MasterCategory.delete(category);
+
+      return new Response.SuccessResponse('', category).use(res);
     } catch (err) {
       next(err);
     }
