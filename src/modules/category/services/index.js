5b857b8c3c64481bc7b55c8788c093c3c3d41b03
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-06 07:22:44 +0700

    add related category service

--- src/modules/category/services/index.js
+++ src/modules/category/services/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   MasterCategory: require('./master.service.js'),
+  RelatedCategory: require('./related.service.js'),
 };
--- src/modules/category/services/master.service.js
+++ src/modules/category/services/master.service.js
@@ -1,15 +1,12 @@
 const Category = require('../model');
-const { optionalObjectProperty } = require('../../../utils');
 const { modelOrId: categoryModelOrId } = require('../helpers');
-const { Service } = require('../../../helpers/class');
+const { Service: CategoryService } = require('../helpers/class');
 
-module.exports = class MasterCategoryService extends Service {
+module.exports = class MasterCategoryService extends CategoryService {
   static async get(options = {}) {
-    const { userId = '' } = options.filter;
-
     return Category.findAll({
       where: {
-        ...optionalObjectProperty('userId', userId),
+        ...MasterCategoryService.filter(options.filter),
       },
     });
   }
@@ -20,14 +17,6 @@ module.exports = class MasterCategoryService extends Service {
 
   static async find(id) {
     const category = await Category.findByPk(id);
-
-    return MasterCategoryService.findOrFail(category);
-  }
-
-  static async findWithTodo(id) {
-    const category = await Category.findByPk(id, {
-      // include: ['todo']
-    });
 
     return MasterCategoryService.findOrFail(category);
   }
