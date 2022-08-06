cef1720577611d5fa399ca4648444b7a529b3a51
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-06 22:05:54 +0700

    add todo filter

---
+++ src/modules/todo/helpers/filter.js
@@ -0,0 +1,37 @@
+const { Op } = require('sequelize');
+const {
+  optionalObjectProperty,
+  sort,
+  paginate,
+  toBoolean,
+} = require('../../../utils');
+
+module.exports = (options = {}) => ({
+  where: {
+    ...optionalObjectProperty(
+      options.filter?.userId,
+      'userId',
+      options.filter.userId
+    ),
+    ...optionalObjectProperty(options.filter?.name, 'name', {
+      [Op.substring]: options.filter.name,
+    }),
+    ...optionalObjectProperty(options.filter?.due, 'due', options.filter.due),
+    ...optionalObjectProperty(
+      options.filter?.categoryId,
+      'categoryId',
+      options.filter.categoryId
+    ),
+    ...optionalObjectProperty(
+      options.filter?.status,
+      'status',
+      toBoolean(options.filter.status)
+    ),
+    ...optionalObjectProperty(options.filter?.late, 'due', {
+      [Op.lt]: options.filter.late,
+    }),
+    ...optionalObjectProperty(options.filter?.late, 'status', false),
+  },
+  ...sort(options),
+  ...paginate(options),
+});
