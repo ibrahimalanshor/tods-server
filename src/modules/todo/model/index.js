4cd84f06913b3788dd2356710816b58905616b0d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-07 11:51:07 +0700

    add children todo model

--- src/modules/todo/model/index.js
+++ src/modules/todo/model/index.js
@@ -1,13 +1,23 @@
+const Exception = require('@ibrahimanshor/express-app/lib/exceptions');
 const Todo = require('./todo.model.js');
 const Category = require('../../category/model/category.model.js');
 const User = require('../../user/model/user.model.js');
+
+Todo.belongsTo(User, {
+  foreignKey: 'userId',
+});
 
 Todo.belongsTo(Category, {
   foreignKey: 'categoryId',
 });
 
-Todo.belongsTo(User, {
-  foreignKey: 'userId',
+Todo.hasMany(Todo, {
+  as: 'children',
+  foreignKey: 'parentId',
 });
+
+Todo.prototype.mustChildren = function () {
+  if (this.parentId === null) throw new Exception.ForbiddenException('');
+};
 
 module.exports = Todo;
