cb5ad782dd50a64753191c7883531f916bdf40c7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-12 06:26:43 +0700

    fix: paginate helper limit is nullable

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,4 +1,6 @@
 module.exports = (options) => {
+  if (!options?.limit && !options?.page) return {};
+
   const limit = +(options?.limit || 10);
   const offset = Math.max(((options?.page || 1) - 1) * limit, 0);
 
