42322c03224dcb25a2377d3d6f1a2bfb05ad2ea9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-12 05:58:15 +0700

    fix: logout must be login

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const ExpressAuth = require('@ibrahimanshor/express-auth');
 const AuthService = require('./services');
 const AuthRequest = require('./requests');
+const { auth } = require('./middlewares');
 
 const router = Router();
 
@@ -16,7 +17,12 @@ router.post(
   AuthRequest.refreshToken,
   ExpressAuth.refreshToken(AuthService)
 );
-router.post('/logout', AuthRequest.logout, ExpressAuth.logout(AuthService));
+router.post(
+  '/logout',
+  auth,
+  AuthRequest.logout,
+  ExpressAuth.logout(AuthService)
+);
 
 module.exports = {
   path: '/api/auth',
